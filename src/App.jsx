0be32365d1930d4294beaa4ae188fe449d36432f
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-01-08 14:50:17 +0100

    MyFooter component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import MyNav from './components/MyNav';
+import MyFooter from './components/MyFooter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <MyNav />
+      <MyFooter />
     </>
   );
 }
